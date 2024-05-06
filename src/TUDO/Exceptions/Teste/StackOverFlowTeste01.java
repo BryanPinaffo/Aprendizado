127549c3f95d8040677fd3cad26ccab94e24bcb5
bryan-pinaffo <bryan.pinaffo.amp@gmail.com>
2024-05-06 13:48:39 -0300

    sla qual commit

---
+++ src/TUDO/Exceptions/Teste/StackOverFlowTeste01.java
@@ -0,0 +1,28 @@
+package TUDO.Exceptions.Teste;
+
+public class StackOverFlowTeste01 {
+    public static void main(String[] args) {
+
+        recursividade();
+        recursividade02();
+
+
+
+    }
+
+
+    public static void recursividade(){
+
+
+            recursividade();
+
+
+    }
+    public static void recursividade02(){
+
+
+            recursividade02();
+
+
+    }
+}
