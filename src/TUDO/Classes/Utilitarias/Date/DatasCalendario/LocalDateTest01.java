949b32458c48cc669d054b1ee95cde805026b1e6
bryan-pinaffo <bryan.pinaffo.amp@gmail.com>
2024-08-16 11:19:57 -0300

    sla qual commit

---
+++ src/TUDO/Classes/Utilitarias/Date/DatasCalendario/LocalDateTest01.java
@@ -0,0 +1,21 @@
+package TUDO.Classes.Utilitarias.Date.DatasCalendario;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+        LocalDate date = LocalDate.of(2024, Month.AUGUST,17);
+        System.out.println(date.getYear());
+        System.out.println(date.getMonth());
+        System.out.println(date.getDayOfMonth());
+        System.out.println(date.getDayOfWeek());
+        System.out.println(date.getDayOfYear());
+
+    }
+}
