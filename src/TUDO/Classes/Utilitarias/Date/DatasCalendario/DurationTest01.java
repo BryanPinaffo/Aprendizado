b110d81c3b48015b61c3779c339dc9c6e8e16bbc
bryan-pinaffo <bryan.pinaffo.amp@gmail.com>
2024-09-13 12:14:26 -0300

    sla qual commit

---
+++ src/TUDO/Classes/Utilitarias/Date/DatasCalendario/DurationTest01.java
@@ -0,0 +1,34 @@
+package TUDO.Classes.Utilitarias.Date.DatasCalendario;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class DurationTest01 {
+
+    public static void main(String[] args) {
+        // duration é uma classe que é usada para pegar o intervalo de tempo
+        //podendo ser ele de anos, meses, dias, horas, segundos e nano
+
+
+        LocalDateTime now = LocalDateTime.now(); // Localdatetame representa a data e a hora combinadas, sem fusos horarios
+                                                //.now() - pega a data e hora atual no momento da execuçao
+        LocalDateTime now2Anos = LocalDateTime.now().minusYears(21); // plusyears adiciona 2 anos a cada data (colocamos o numero 2)
+        LocalTime timeNow = LocalTime.now(); // representa a hora (horas, minutos, segudos e nanosegundos), sem a data
+        LocalTime timeMinus7Horas = LocalTime.now().minusHours(7); // retira 7 horas do horario atual
+        Duration d1 = Duration.between(now2Anos,now); // duration: quantidade de tempo entre dois instantes,
+                                                        // com a diferença entre duas datas ou dois horarios
+        Duration d2 = Duration.between(timeNow,timeMinus7Horas);
+        Duration d3 = Duration.between(Instant.now(),Instant.MIN);
+        // localdate nao da suporte para segundos, entao ele daria erro se um pedir segundos.
+
+        Duration d4 = Duration.ofDays(23);
+        // ele nao trabalha com dias, apenas com horas, minutos e segundos
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(d4);
+
+    }
+}
---
+++ src/TUDO/Classes/Utilitarias/Date/DatasCalendario/PeriodTest01.java
@@ -0,0 +1,7 @@
+package TUDO.Classes.Utilitarias.Date.DatasCalendario;
+
+public class PeriodTest01 {
+    public static void main(String[] args) {
+
+    }
+}
