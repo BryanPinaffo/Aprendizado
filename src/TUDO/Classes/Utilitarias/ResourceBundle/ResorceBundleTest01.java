e2e339420faed892329e04e8e737ef7e2033b1aa
BryanPinaffo <bryan.pinaffo.amp@gmail.com>
2025-02-08 17:08:07 -0300

    Aprendendo Classes utilitarias:  Personalizando ResorceBundleTest01

--- src/TUDO/Classes/Utilitarias/ResourceBundle/ResorceBundleTest01.java
+++ src/TUDO/Classes/Utilitarias/ResourceBundle/ResorceBundleTest01.java
@@ -1,5 +1,6 @@
 package TUDO.Classes.Utilitarias.ResourceBundle;
 
+import javax.swing.*;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -8,20 +9,24 @@ public class ResorceBundleTest01 {
     // forma que o java tem de pegar uma mensagem baseada em um local
     public static void main(String[] args) {
 
+        String dicionario = JOptionPane.showInputDialog("Digite seu a palavra com .: ");
+
         System.out.println(Locale.getDefault());
         ResourceBundle bundle = ResourceBundle.getBundle("message", new Locale("pt", "BR"));
-        bundle.getString("hello");
-        System.out.println(bundle.getString("hello"));
-        System.out.println(bundle.getString("good.morning"));
-
-        System.out.println(bundle.getString("hi")); // nesse caso, vai como lingua geral, ja que o message nao foi especificado, e o hi só existe nesse properties
-
-        bundle = ResourceBundle.getBundle("message",new Locale("en","US"));
-        System.out.println(bundle.getString("hello"));
-        System.out.println(bundle.getString("good.morning"));
+        //bundle.getString("hello");
+        System.out.println(bundle.getString(dicionario));
 
 
-        System.out.println(bundle.getString("hi"));
+        //System.out.println(bundle.getString("good.morning"));
+
+       // System.out.println(bundle.getString("hi")); // nesse caso, vai como lingua geral, ja que o message nao foi especificado, e o hi só existe nesse properties
+
+       // bundle = ResourceBundle.getBundle("message",new Locale("en","US"));
+       // System.out.println(bundle.getString("hello"));
+        //System.out.println(bundle.getString("good.morning"));
+
+
+        //System.out.println(bundle.getString("hi"));
 
         //Locale("fr","CA")
 
